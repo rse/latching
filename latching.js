adbb4388afbac6e8d7b0261ebef3ff070162431b
Ralf S. Engelschall <rse@engelschall.com>
2021-01-09 10:32:24 +0100

    bump year in all copyright messages

--- latching.js
+++ latching.js
@@ -1,6 +1,6 @@
 /*
 **  Latching -- Run-Time Hook Latching
-**  Copyright (c) 2012-2020 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**  Copyright (c) 2012-2021 Dr. Ralf S. Engelschall <rse@engelschall.com>
 **
 **  Permission is hereby granted, free of charge, to any person obtaining
 **  a copy of this software and associated documentation files (the
